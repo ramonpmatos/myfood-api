77903dbb24e75ff3226d443fff08a452f399d688
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-29 17:38:30 -0300

    Criando as tabelas do banco a partir da entidades

--- src/main/java/com/delivery/myfood/domain/model/Restaurante.java
+++ src/main/java/com/delivery/myfood/domain/model/Restaurante.java
@@ -11,6 +11,8 @@ public class Restaurante {
 	@Id
 	private Long id;
 	
+	private String nome;
+	
 	private BigDecimal taxaFrete;
 
 	public Long getId() {
@@ -19,6 +21,14 @@ public class Restaurante {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public BigDecimal getTaxaFrete() {
