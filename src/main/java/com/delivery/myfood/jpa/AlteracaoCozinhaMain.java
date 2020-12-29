bb58ab5d466009c3005f604285a8510e11b0fb8a
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-29 18:32:41 -0300

    Atualizando um objeto no banco de dados

--- src/main/java/com/delivery/myfood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/delivery/myfood/jpa/AlteracaoCozinhaMain.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import com.delivery.myfood.MyfoodApiApplication;
 import com.delivery.myfood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(MyfoodApiApplication.class)
@@ -16,17 +16,12 @@ public class InclusaoCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brazileira");
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		cozinha.setNome("Brazileira");
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
+		cadastroCozinha.salvar(cozinha);
 		
-		cozinha1 = cadastroCozinha.salvar(cozinha1);
-		cozinha2 = cadastroCozinha.salvar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
 		
 	}
 
