8e36bb61c3c423012e572abd8ed9b7417c612d68
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-29 16:14:38 -0300

    Commit inical do curso Especialista Spring Rest

---
+++ src/test/java/com/delivery/myfood/MyfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
