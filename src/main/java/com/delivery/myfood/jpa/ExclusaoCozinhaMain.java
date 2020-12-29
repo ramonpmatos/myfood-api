0bb82dae7c377f26ea0e5d5ccc7bcc7b2cf0e7ef
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-29 18:40:30 -0300

    Excluindo um objeto do banco de dados

---
+++ src/main/java/com/delivery/myfood/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.delivery.myfood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.delivery.myfood.MyfoodApiApplication;
+import com.delivery.myfood.domain.model.Cozinha;
+
+public class ExclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(MyfoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		
+		cadastroCozinha.remover(cozinha);
+	
+	}
+
+}
