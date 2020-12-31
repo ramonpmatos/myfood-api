0fc9e6db46c7b51d0e537b0cdcdf640bf8c80bf4
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-31 19:12:53 -0300

    Manipulando a resposta HTTP com ResponeEntity

--- src/main/java/com/delivery/myfood/api/controller/CozinhaController.java
+++ src/main/java/com/delivery/myfood/api/controller/CozinhaController.java
@@ -3,11 +3,12 @@ package com.delivery.myfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.myfood.domain.model.Cozinha;
@@ -25,10 +26,21 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
-	@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
+		
 	}
 
 }
