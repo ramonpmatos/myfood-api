ce84d16e229525dab33135fd57f4ec7651ee9760
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-31 18:21:44 -0300

    Custuminzando as presentações XML e JSON

--- src/main/java/com/delivery/myfood/domain/model/Cozinha.java
+++ src/main/java/com/delivery/myfood/domain/model/Cozinha.java
@@ -5,9 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+//@JsonRootName("gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -18,6 +23,8 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+//	@JsonIgnore
+	@JsonProperty("titulo")
 	private String nome;
 	
 	
