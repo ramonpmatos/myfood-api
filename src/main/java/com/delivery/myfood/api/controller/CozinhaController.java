2e529e74f73785393234c13f6be4f195fd75571f
ramonpmatos <ramonpereira.10@gmail.com>
2020-12-31 18:59:38 -0300

    Definindo o status da reposta HTTP com @ResponseStatus

--- src/main/java/com/delivery/myfood/api/controller/CozinhaController.java
+++ src/main/java/com/delivery/myfood/api/controller/CozinhaController.java
@@ -3,9 +3,11 @@ package com.delivery.myfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.myfood.domain.model.Cozinha;
@@ -23,6 +25,7 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
+	@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
 	public Cozinha buscar(@PathVariable Long cozinhaId) {
 		return cozinhaRepository.buscar(cozinhaId);
