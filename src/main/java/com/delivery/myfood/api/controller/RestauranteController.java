f498569b2725485ad78cb2007ff45ad8b17ad324
ramonpmatos <ramonpereira.10@gmail.com>
2021-01-02 15:26:35 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/com/delivery/myfood/api/controller/RestauranteController.java
+++ src/main/java/com/delivery/myfood/api/controller/RestauranteController.java
@@ -3,14 +3,20 @@ package com.delivery.myfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.delivery.myfood.domain.exception.EntidadeNaoEncontradaException;
 import com.delivery.myfood.domain.model.Restaurante;
 import com.delivery.myfood.domain.repository.RestauranteRepository;
+import com.delivery.myfood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -19,14 +25,17 @@ public class RestauranteController {
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
+
 	@GetMapping
 	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
 	}
-	
+
 	@GetMapping("/{restauranteId}")
 	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
-		
+
 		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
 
 		if (restaurante != null) {
@@ -34,6 +43,21 @@ public class RestauranteController {
 		}
 
 		return ResponseEntity.notFound().build();
-		
+
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			restaurante = cadastroRestaurante.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(restaurante);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
 	}
 }
