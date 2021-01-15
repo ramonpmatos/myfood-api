28a30d6a149495ae161898ac0e0bedaad6e6647b
ramonpmatos <ramonpereira.10@gmail.com>
2021-01-14 21:49:29 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/delivery/myfood/domain/model/Cozinha.java
+++ src/main/java/com/delivery/myfood/domain/model/Cozinha.java
@@ -1,9 +1,15 @@
 package com.delivery.myfood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,7 +23,11 @@ public class Cozinha {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nome;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
+
 }
