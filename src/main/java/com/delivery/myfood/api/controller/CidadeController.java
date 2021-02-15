8bf09d27a5c8f1571544bcf625e39f7f7e47cec7
ramonpmatos <ramonpereira.10@gmail.com>
2021-02-15 19:34:11 -0300

    Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/delivery/myfood/api/controller/CidadeController.java
+++ src/main/java/com/delivery/myfood/api/controller/CidadeController.java
@@ -1,14 +1,11 @@
 package com.delivery.myfood.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.delivery.myfood.api.exceptionhandler.Problema;
-import com.delivery.myfood.domain.exception.EntidadeNaoEncontradaException;
 import com.delivery.myfood.domain.exception.EstadoNaoEncontradoException;
 import com.delivery.myfood.domain.exception.NegocioException;
 import com.delivery.myfood.domain.model.Cidade;
@@ -74,23 +69,4 @@ public class CidadeController {
 		cadastroCidade.excluir(cidadeId);
 	}
 	
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidaEntidadeNaoEncontradaException(
-			EntidadeNaoEncontradaException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
-	}
-	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(
-			EntidadeNaoEncontradaException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
-	}
 }
---
+++ src/main/java/com/delivery/myfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.delivery.myfood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.delivery.myfood.domain.exception.EntidadeNaoEncontradaException;
+import com.delivery.myfood.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidaEntidadeNaoEncontradaException(
+			EntidadeNaoEncontradaException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(
+			EntidadeNaoEncontradaException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
+
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<?> tratarNegocioException(){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("O tipo de mídia não é aceito.").build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
+}
