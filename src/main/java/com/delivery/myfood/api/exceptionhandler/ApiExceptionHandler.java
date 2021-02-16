ca649455a0c9af58605905a4c42386093d77f4eb
ramonpmatos <ramonpereira.10@gmail.com>
2021-02-16 14:54:27 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/delivery/myfood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/delivery/myfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.delivery.myfood.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +15,18 @@ import com.delivery.myfood.domain.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "O corpo da requisição está invalido. Verifique erro de sintaxe.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, null, status, request);
+	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidaEntidadeNaoEncontradaException(
