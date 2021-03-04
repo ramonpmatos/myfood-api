f375da1543c4781df5dac63e6d77c6b6388da5dc
ramonpmatos <ramonpereira.10@gmail.com>
2021-03-04 18:03:16 -0300

    Conhecendo e adicionando mais constraints de validação no modelo

--- src/main/java/com/delivery/myfood/domain/model/Restaurante.java
+++ src/main/java/com/delivery/myfood/domain/model/Restaurante.java
@@ -16,13 +16,13 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -37,10 +37,12 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotNull
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
+//	@DecimalMin("0")
+	@PositiveOrZero
 	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 
